7dd63851ee67793f285e407969b4f3a40378420e
梁灏 <admin@aresn.com>
2017-08-13 17:36:06 +0800

    update i18n

--- src/views/guide/i18n-en.vue
+++ src/views/guide/i18n-en.vue
@@ -23,7 +23,10 @@
                 <li>French(fr-FR)</li>
                 <li>German(de-DE)</li>
                 <li>Brazilian Portuguese(pt-BR)</li>
+                <li>Portuguese(pt-PT)</li>
                 <li>Korean(ko-KR)</li>
+                <li>Vietnamese(vi-VN)</li>
+                <li>Swedish(sv-SE)</li>
             </ul>
             <p>If your target language is not included, you are more than welcome to contribute: <a href="https://github.com/iview/iview/tree/2.0/src/locale/lang" target="_blank">Supported Languages</a></p>
         </article>
--- src/views/guide/i18n.vue
+++ src/views/guide/i18n.vue
@@ -22,7 +22,10 @@
                 <li>法语（fr-FR）</li>
                 <li>德语（de-DE）</li>
                 <li>巴西葡萄牙语（pt-BR）</li>
+                <li>葡萄牙语（pt-PT）</li>
                 <li>韩语（ko-KR）</li>
+                <li>越南语（vi-VN）</li>
+                <li>瑞典语（sv-SE）</li>
             </ul>
             <p>欢迎贡献代码，以支持更多语言：<a href="https://github.com/iview/iview/tree/2.0/src/locale/lang" target="_blank">查看</a></p>
         </article>
