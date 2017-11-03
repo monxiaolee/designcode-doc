f7b297be35e4e196b0296707df670036c7cf8b1e
梁灏 <admin@aresn.com>
2017-11-03 17:17:44 +0800

    translate Carousel examples

--- src/views/components/carousel-en.vue
+++ src/views/components/carousel-en.vue
@@ -62,39 +62,39 @@
             <Demo title="Integrated Properties">
                 <div slot="demo">
                     <Form :model="setting" :label-width="100">
-                        <FormItem label="自动切换">
+                        <FormItem label="Automatic switching">
                             <Switch v-model="setting.autoplay">
-                                <span slot="open">开</span>
-                                <span slot="close">关</span>
+                                <span slot="open">On</span>
+                                <span slot="close">Off</span>
                             </Switch>
                         </FormItem>
-                        <FormItem label="圆形指示器">
+                        <FormItem label="Circular indicator">
                             <Switch v-model="setting.radiusDot">
-                                <span slot="open">开</span>
-                                <span slot="close">关</span>
+                                <span slot="open">On</span>
+                                <span slot="close">Off</span>
                             </Switch>
                         </FormItem>
-                        <FormItem label="自动切换速度">
+                        <FormItem label="Automatic switching speed">
                             <Slider v-model="setting.autoplaySpeed" :min="300" :max="10000" :step="100"></Slider>
                         </FormItem>
-                        <FormItem label="指示器位置">
+                        <FormItem label="Indicator position">
                             <RadioGroup v-model="setting.dots" type="button">
-                                <Radio label="inside">内部</Radio>
-                                <Radio label="outside">外部</Radio>
-                                <Radio label="none">不显示</Radio>
+                                <Radio label="inside">interior</Radio>
+                                <Radio label="outside">exterior</Radio>
+                                <Radio label="none">Don't show</Radio>
                             </RadioGroup>
                         </FormItem>
-                        <FormItem label="切换箭头">
+                        <FormItem label="Switch the arrow">
                             <RadioGroup v-model="setting.arrow" type="button">
-                                <Radio label="hover">悬停时显示</Radio>
-                                <Radio label="always">一直显示</Radio>
-                                <Radio label="never">不显示</Radio>
+                                <Radio label="hover">Hover displayed when</Radio>
+                                <Radio label="always">Always show</Radio>
+                                <Radio label="never">Don't show</Radio>
                             </RadioGroup>
                         </FormItem>
-                        <FormItem label="指示器触发方式">
+                        <FormItem label="Indicator trigger mode">
                             <RadioGroup v-model="setting.trigger" type="button">
-                                <Radio label="click">点击</Radio>
-                                <Radio label="hover">悬停</Radio>
+                                <Radio label="click">Click</Radio>
+                                <Radio label="hover">Hover</Radio>
                             </RadioGroup>
                         </FormItem>
                     </Form>
