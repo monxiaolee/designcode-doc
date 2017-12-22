4d67781e48c61ab9138e70f624db7485b863b1d4
zhigang.li <zhigang.li@tendcloud.com>
2017-12-22 17:25:46 +0800

    update layout menu effect

--- src/code/layout.js
+++ src/code/layout.js
@@ -338,58 +338,80 @@ code.left = `
         height: 100%;
         width: 100%;
     }
+    .menu-item span{
+        display: inline-block;
+        overflow: hidden;
+        width: 69px;
+        text-overflow: ellipsis;
+        white-space: nowrap;
+        vertical-align: bottom;
+        transition: all .2s ease .2s;
+    }
+    .menu-item i{
+        transform: translateX(0px);
+        transition: all .2s ease;
+        vertical-align: middle;
+        font-size: 16px;
+    }
+    .collapsed-menu span{
+        width: 0px;
+        transition: all .2s ease;
+    }
+    .collapsed-menu i{
+        transform: translateX(5px);
+        transition: all .2s ease .2s;
+        vertical-align: middle;
+        font-size: 22px;
+    }
 </style>
 <template>
     <div class="layout">
-        <Layout :style="{minHeight: '100vh'}">
-            <Sider collapsible>
-                <Menu active-name="1-2" theme="dark" width="auto" :open-names="['1']">
-                    <Submenu name="1">
-                        <template slot="title">
-                            <Icon type="ios-navigate"></Icon>
-                            Item 1
-                        </template>
-                        <MenuItem name="1-1">Option 1</MenuItem>
-                        <MenuItem name="1-2">Option 2</MenuItem>
-                        <MenuItem name="1-3">Option 3</MenuItem>
-                    </Submenu>
-                    <Submenu name="2">
-                        <template slot="title">
-                            <Icon type="ios-keypad"></Icon>
-                            Item 2
-                        </template>
-                        <MenuItem name="2-1">Option 1</MenuItem>
-                        <MenuItem name="2-2">Option 2</MenuItem>
-                    </Submenu>
-                    <Submenu name="3">
-                        <template slot="title">
-                            <Icon type="ios-analytics"></Icon>
-                            Item 3
-                        </template>
-                        <MenuItem name="3-1">Option 1</MenuItem>
-                        <MenuItem name="3-2">Option 2</MenuItem>
-                    </Submenu>
-                </Menu>
-            </Sider>
-            <Layout>
-                <Header :style="{background: '#fff', boxShadow: '0 2px 3px 2px rgba(0,0,0,.1)'}"></Header>
-                <Content :style="{padding: '0 16px 16px'}">
-                    <Breadcrumb :style="{margin: '16px 0'}">
-                        <BreadcrumbItem>Home</BreadcrumbItem>
-                        <BreadcrumbItem>Components</BreadcrumbItem>
-                        <BreadcrumbItem>Layout</BreadcrumbItem>
-                    </Breadcrumb>
-                    <Card>
-                        <div style="height: 600px">Content</div>
-                    </Card>
-                </Content>
-            </Layout>
+        <Sider collapsible :collapsed-width="78" v-model="isCollapsed">
+            <Menu active-name="1-2" theme="dark" width="auto" :class="menuitemClasses">
+                <MenuItem name="1-1">
+                    <Icon type="ios-navigate"></Icon>
+                    <span>Option 1</span>
+                </MenuItem>
+                <MenuItem name="1-2">
+                    <Icon type="search"></Icon>
+                    <span>Option 2</span>
+                </MenuItem>
+                <MenuItem name="1-3">
+                    <Icon type="settings"></Icon>
+                    <span>Option 3</span>
+                </MenuItem>
+            </Menu>
+        </Sider>
+        <Layout>
+            <Header :style="{background: '#fff', boxShadow: '0 2px 3px 2px rgba(0,0,0,.1)'}"></Header>
+            <Content :style="{padding: '0 16px 16px'}">
+                <Breadcrumb :style="{margin: '16px 0'}">
+                    <BreadcrumbItem>Home</BreadcrumbItem>
+                    <BreadcrumbItem>Components</BreadcrumbItem>
+                    <BreadcrumbItem>Layout</BreadcrumbItem>
+                </Breadcrumb>
+                <Card>
+                    <div style="height: 600px">Content</div>
+                </Card>
+            </Content>
         </Layout>
     </div>
 </template>
 <script>
     export default {
-        
+        data () {
+            return {
+                isCollapsed: false
+            };
+        },
+        computed: {
+            menuitemClasses: function () {
+                return [
+                    'menu-item',
+                    this.isCollapsed ? 'collapsed-menu' : ''
+                ]
+            }
+        }
     }
 </script>
 `;
@@ -424,19 +446,27 @@ code.selfTrigger = `
 <template>
     <div class="layout">
         <Layout>
-            <Sider ref="side1" hide-trigger collapsible v-model="isCollapsed">
-                <Menu active-name="1-2" theme="dark" width="auto" :open-names="['1']">
-                    <div class="layout-logo-left"></div>
-                    <MenuItem name="1-1"><Icon type="ios-navigate"></Icon>Option 1</MenuItem>
-                    <MenuItem name="1-2"><Icon type="ios-keypad"></Icon>Option 2</MenuItem>
-                    <MenuItem name="1-3"><Icon type="ios-analytics"></Icon>Option 3</MenuItem>
+            <Sider ref="side1" hide-trigger collapsible :collapsed-width="78" v-model="isCollapsed">
+                <Menu active-name="1-2" theme="dark" width="auto" :class="menuitemClasses">
+                    <MenuItem name="1-1">
+                        <Icon type="ios-navigate"></Icon>
+                        <span>Option 1</span>
+                    </MenuItem>
+                    <MenuItem name="1-2">
+                        <Icon type="search"></Icon>
+                        <span>Option 2</span>
+                    </MenuItem>
+                    <MenuItem name="1-3">
+                        <Icon type="settings"></Icon>
+                        <span>Option 3</span>
+                    </MenuItem>
                 </Menu>
             </Sider>
             <Layout>
                 <Header :style="{padding: 0}" class="layout-header-bar">
                     <Icon @click.native="collapsedSider" :class="rotateIcon" :style="{margin: '20px 20px 0'}" type="navicon-round" size="24"></Icon>
                 </Header>
-                <Content :style="{margin: '20px', background: '#fff', minHeight: '200px'}">
+                <Content :style="{margin: '20px', background: '#fff', minHeight: '260px'}">
                     Content
                 </Content>
             </Layout>
@@ -456,6 +486,12 @@ code.selfTrigger = `
                     'menu-icon',
                     this.isCollapsed ? 'rotate-icon' : ''
                 ];
+            },
+            menuitemClasses () {
+                return [
+                    'menu-item',
+                    this.isCollapsed ? 'collapsed-menu' : ''
+                ]
             }
         },
         methods: {
@@ -479,23 +515,49 @@ code.media = `
         background: #fff;
         box-shadow: 0 1px 1px rgba(0,0,0,.1);
     }
-    .layout-logo-left{
-        width: 90%;
-        height: 30px;
-        background: #5b6270;
-        border-radius: 3px;
-        margin: 15px auto;
+    .menu-item span{
+        display: inline-block;
+        overflow: hidden;
+        width: 69px;
+        text-overflow: ellipsis;
+        white-space: nowrap;
+        vertical-align: bottom;
+        transition: all .2s ease .2s;
+    }
+    .menu-item i{
+        transform: translateX(0px);
+        transition: all .2s ease;
+        vertical-align: middle;
+        font-size: 16px;
+    }
+    .collapsed-menu span{
+        width: 0px;
+        transition: all .2s ease;
+    }
+    .collapsed-menu i{
+        transform: translateX(5px);
+        transition: all .2s ease .2s;
+        vertical-align: middle;
+        font-size: 22px;
     }
 </style>
 <template>
     <div class="layout">
         <Layout>
-            <Sider breakpoint="md" collapsible>
-                <Menu active-name="1-2" theme="dark" width="auto" :open-names="['1']">
-                    <div class="layout-logo-left"></div>
-                    <MenuItem name="1-1"><Icon type="ios-navigate"></Icon>Option 1</MenuItem>
-                    <MenuItem name="1-2"><Icon type="ios-keypad"></Icon>Option 2</MenuItem>
-                    <MenuItem name="1-3"><Icon type="ios-analytics"></Icon>Option 3</MenuItem>
+            <Sider breakpoint="md" collapsible :collapsed-width="78" v-model="isCollapsed">
+                <Menu active-name="1-2" theme="dark" width="auto" :class="menuitemClasses">
+                    <MenuItem name="1-1">
+                        <Icon type="ios-navigate"></Icon>
+                        <span>Option 1</span>
+                    </MenuItem>
+                    <MenuItem name="1-2">
+                        <Icon type="search"></Icon>
+                        <span>Option 2</span>
+                    </MenuItem>
+                    <MenuItem name="1-3">
+                        <Icon type="settings"></Icon>
+                        <span>Option 3</span>
+                    </MenuItem>
                 </Menu>
                 <div slot="trigger"></div>
             </Sider>
@@ -510,7 +572,19 @@ code.media = `
 </template>
 <script>
     export default {
-        
+        data () {
+            return {
+                isCollapsed: false
+            };
+        },
+        computed: {
+            menuitemClasses: function () {
+                return [
+                    'menu-item',
+                    this.isCollapsed ? 'collapsed-menu' : ''
+                ]
+            }
+        }
     }
 </script>
 `
