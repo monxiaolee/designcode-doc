b08569be9b4b348de88bc5c1e8b038821b71c0af
梁灏 <admin@aresn.com>
2018-02-07 14:40:45 +0800

    update

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,13 +1,25 @@
 <style scoped lang="less">
-    @import "../styles/index.less";
+    @import "../styles/index-2018.less";
 </style>
 <style>
-    .index-version .ivu-badge-count{
+    .index-version .ivu-badge-count {
         box-shadow: none;
         right: 0;
         top: -3px;
         color: #f50;
         background: transparent;
+    }
+    .fire {
+        width: 3px;
+        height: 30px;
+        background: white;
+        position: absolute;top:100%;
+    }
+    .spark {
+        position: absolute;
+        width: 10px;
+        height: 10px;
+        border-radius: 50%;
     }
 </style>
 <template>
@@ -16,21 +28,20 @@
             <div class="bg"><span></span></div>
             <Row type="flex" justify="center" align="middle" style="position: relative;z-index: 3">
                 <i-col span="24">
-                    <h1>
-                        <img src="../images/logo.png" class="img-logo">
-                        <img src="../images/name.png" class="img-name">
-                    </h1>
-                    <h2>{{ $t('index.title') }}</h2>
+                    <h2>
+                        <canvasText key="canvas1" cid="canvas1" content="新年快乐" v-show="index % 2 !== 0"></canvasText>
+                        <canvasText key="canvas2" cid="canvas2" content="iView UI"
+                                    v-show="index % 2 === 0"></canvasText>
+                    </h2>
                     <div class="list">
-                        <router-link :to="'/docs/guide/introduce' + suffix">{{ $t('index.guide') }}</router-link>
-                        <router-link :to="'/docs/guide/install' + suffix">{{ $t('index.component') }}</router-link>
-                        <router-link :to="'/docs/practice/case' + suffix">{{ $t('index.practice') }}</router-link>
-                        <router-link :to="'/overview' + suffix">{{ $t('index.overview') }}</router-link>
-                        <router-link :to="'/cli' + suffix">{{ $t('index.cli') }}</router-link>
-                        <a href="https://github.com/iview/iview" target="_blank">
-                            <Icon type="social-github"></Icon>
-                            GitHub
-                        </a>
+                        <Button size="large" shape="circle" type="primary" style="min-width: 130px" icon="ios-keypad"
+                                @click="handleStart">
+                            <template v-if="lang === 'zh-CN'">开始使用</template>
+                            <template v-else>Getting Started</template>
+                        </Button>
+                        <Button size="large" shape="circle" type="success" style="min-width: 130px" icon="social-github"
+                                @click="handleGithub">GitHub
+                        </Button>
                     </div>
                 </i-col>
             </Row>
@@ -42,16 +53,21 @@
                 <template v-else>中文</template>
             </span>
         </div>
+        <!--<div class="index-logo-new-year">-->
+        <!--<img src="../images/logo-doc-2018.png">-->
+        <!--</div>-->
     </div>
 </template>
 <script>
-    import THREE from '../libs/three/three';
     import bus from '../../src/components/bus';
+    import canvasText from '../../src/components/canvas-text.vue';
 
     export default {
+        components: {canvasText},
         data () {
             return {
-                lang: this.$lang
+                lang: this.$lang,
+                index: 0
             }
         },
         computed: {
@@ -60,124 +76,31 @@
             }
         },
         methods: {
-            liziInit () {
-                var SCREEN_WIDTH = window.innerWidth;
-                var SCREEN_HEIGHT = window.innerHeight;
-
-                var SEPARATION = 90;
-                var AMOUNTX = 50;
-                var AMOUNTY = 50;
-
-                var container;
-
-                var particles, particle;
-                var count;
-
-                var camera;
-                var scene;
-                var renderer;
-
-                var mouseX = 0;
-                var mouseY = 0;
-
-                var windowHalfX = window.innerWidth / 2;
-                var windowHalfY = window.innerHeight / 2;
-
-                init();
-                this.interval = setInterval(loop, 1000 / 60);
-
-                function init() {
-
-                    container = document.createElement( 'div' );
-                    container.style.position = 'relative';
-                    container.style.top = '200px';
-                    document.getElementById('indexLizi').appendChild( container );
-
-                    camera = new THREE.Camera( 75, SCREEN_WIDTH / SCREEN_HEIGHT, 1, 10000 );
-                    camera.position.z = 1000;
-
-                    scene = new THREE.Scene();
-
-                    renderer = new THREE.CanvasRenderer();
-                    renderer.setSize( SCREEN_WIDTH, SCREEN_HEIGHT );
-
-                    particles = new Array();
-
-                    var i = 0;
-                    var material = new THREE.ParticleCircleMaterial( 0x097bdb, 1 );
-
-                    for ( var ix = 0; ix < AMOUNTX; ix ++ ) {
-
-                        for ( var iy = 0; iy < AMOUNTY; iy ++ ) {
-
-                            particle = particles[ i ++ ] = new THREE.Particle( material );
-                            particle.position.x = ix * SEPARATION - ( ( AMOUNTX * SEPARATION ) / 2 );
-                            particle.position.z = iy * SEPARATION - ( ( AMOUNTY * SEPARATION ) / 2 );
-                            scene.add( particle );
-                        }
-                    }
-
-                    count = 0;
-
-                    container.appendChild( renderer.domElement );
-
-                    document.addEventListener( 'mousemove', onDocumentMouseMove, false );
-//                    document.addEventListener( 'touchmove', onDocumentTouchMove, false );
-                }
-
-                function onDocumentMouseMove( event ) {
-
-                    mouseX = event.clientX - windowHalfX;
-                    mouseY = event.clientY - windowHalfY;
-
-                }
-
-                function onDocumentTouchMove( event ) {
-
-                    if ( event.touches.length == 1 ) {
-
-                        event.preventDefault();
-
-                        mouseX = event.touches[ 0 ].pageX - windowHalfX;
-                        mouseY = event.touches[ 0 ].pageY - windowHalfY;
-
-                    }
-                }
-
-                function loop() {
-                    camera.position.x += ( mouseX - camera.position.x ) * .05;
-//                    camera.position.y += ( - mouseY - camera.position.y ) * .05;
-                    camera.position.y = 364;
-
-                    var i = 0;
-
-                    for ( var ix = 0; ix < AMOUNTX; ix ++ ) {
-
-                        for ( var iy = 0; iy < AMOUNTY; iy ++ ) {
-
-                            particle = particles[ i++ ];
-                            particle.position.y = ( Math.sin( ( ix + count ) * 0.3 ) * 50 ) + ( Math.sin( ( iy + count ) * 0.5 ) * 50 );
-                            particle.scale.x = particle.scale.y = ( Math.sin( ( ix + count ) * 0.3 ) + 1 ) * 2 + ( Math.sin( ( iy + count ) * 0.5 ) + 1 ) * 2;
-
-                        }
-                    }
-
-                    renderer.render( scene, camera );
-
-                    count += 0.1;
-                }
-            },
             handleChangeLang () {
                 const lang = this.lang === 'zh-CN' ? 'en-US' : 'zh-CN';
                 bus.$emit('on-change-lang', lang, '/');
+            },
+            nextIndex () {
+                if (this.timmer) clearTimeout(this.timmer);
+                this.index++;
+                this.timmer = setTimeout(() => {
+                    this.nextIndex();
+                }, 3000);
+            },
+            handleStart () {
+                this.$router.push('/docs/guide/install');
+            },
+            handleGithub () {
+                window.open('https://github.com/iview/iview');
             }
         },
         mounted () {
             this.lang = this.$lang;
-            this.liziInit();
+            this.nextIndex();
+
         },
         beforeDestroy () {
-            if (this.interval) clearInterval(this.interval);
+            if (this.timmer) clearTimeout(this.timmer);
         }
     }
 </script>
